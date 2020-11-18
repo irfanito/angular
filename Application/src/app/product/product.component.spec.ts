10c5c5afafc9fa3ae0ac422da51d0590e40fa7fe
SALIM Irfane <irfane.salim@generali.com>
2020-11-18 08:20:44 -0800

    Fix product component test

--- Application/src/app/product/product.component.spec.ts
+++ Application/src/app/product/product.component.spec.ts
@@ -16,6 +16,12 @@ describe('ProductComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    component.data = {
+      title: 'Men Sweatshirt',
+      description: 'C0D1NG_TH3_W0RLD BIO HOODIE - MEN',
+      photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
+      price: 39
+    };
     fixture.detectChanges();
   });
 
