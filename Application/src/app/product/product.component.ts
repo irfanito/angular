64b6053ae2e2975a6ea5c88f4ec39a2d31711f24
SALIM Irfane <irfane.salim@generali.com>
2020-11-18 08:04:29 -0800

    Extract product component

--- Application/src/app/product/product.component.ts
+++ Application/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../model/model/product';
 
 @Component({
   selector: 'app-product',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  @Input() data: Product;
+  
   constructor() { }
 
   ngOnInit(): void {
