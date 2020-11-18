904efd255b2b9bbb061b95a44b5c8b11b9cf8357
SALIM Irfane <irfane.salim@generali.com>
2020-11-18 07:35:55 -0800

    Add product's data in app component

--- Application/src/app/app.component.ts
+++ Application/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './model/model/product';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'This is my first component';
   total: number;
+  products: Product[];
   
   constructor(){
     this.total = 0;
+    this.products = AppComponent.defaultProducts;
   }
+
+  static defaultProducts = [{
+    title: 'Men Sweatshirt',
+    description: 'C0D1NG_TH3_W0RLD BIO HOODIE - MEN',
+    photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
+    price: 39
+  },
+  {
+    title: 'Men T-Shirt',
+    description: 'BIO T-SHIRT WITH CREWNECK - MEN.',
+    photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b2911e4ab33424aec592bd6/Mockups/front.png',
+    price: 19
+  },
+  {
+    title: 'T-Shirt women',
+    description: 'BIO T-SHIRT WITH CREWNECK - WOMEN.',
+    photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b290d26ab33424aec592bd4/Mockups/front.png',
+    price: 30
+  },
+  {
+    title: 'Tote bag',
+    description: 'C0D1NG_TH3_W0RLD, BIO TOTE BAG.',
+    photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf160814006a7fe670e2dd/Mockups/front.png',
+    price: 12.5
+  }];
 }
