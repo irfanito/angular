5caebb835698a7bec749ac35303ffe48a33cc1b0
SALIM Irfane <irfane.salim@generali.com>
2020-11-18 06:13:38 -0800

    Delete obsolete app component's test case

--- Application/src/app/app.component.spec.ts
+++ Application/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('This is my first component');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Application app is running!');
-  });
 });
