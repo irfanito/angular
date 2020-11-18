9c3a581c8b81737e71836ed52b25a9413ff807e0
SALIM Irfane <irfane.salim@generali.com>
2020-11-18 08:43:31 -0800

    Test app component's updatePrice method

--- Application/src/app/app.component.spec.ts
+++ Application/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Product } from './model/model/product';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -22,5 +23,22 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.total).toEqual(0);
+  });
+
+  it(`updatePrice should return 10 when total 0 and produc price 39`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    // given
+    const product: Product = {
+      title: 'Men Sweatshirt',
+      description: 'C0D1NG_TH3_W0RLD BIO HOODIE - MEN',
+      photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
+      price: 39
+    };
+    // when
+    app.updatePrice(product);
+    // then
+    expect(app.total).toEqual(39);
   });
 });
