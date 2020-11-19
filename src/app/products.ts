1577de60776a416935d2b3b7704623253ce00e40
SALIM Irfane <irfane.salim@generali.com>
2020-11-19 07:57:48 -0800

    Use sortByTitle pipe

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,12 @@
-export const defaultProducts = [{
+export const defaultProducts = [
+  {
+    title: 'T-Shirt women',
+    description: 'BIO T-SHIRT WITH CREWNECK - WOMEN.',
+    photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b290d26ab33424aec592bd4/Mockups/front.png',
+    price: 30,
+    stock: 6
+  },
+  {
     title: 'Men Sweatshirt',
     description: 'C0D1NG_TH3_W0RLD BIO HOODIE - MEN',
     photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
@@ -11,13 +19,6 @@ export const defaultProducts = [{
     photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b2911e4ab33424aec592bd6/Mockups/front.png',
     price: 19,
     stock: 4
-  },
-  {
-    title: 'T-Shirt women',
-    description: 'BIO T-SHIRT WITH CREWNECK - WOMEN.',
-    photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b290d26ab33424aec592bd4/Mockups/front.png',
-    price: 30,
-    stock: 6
   },
   {
     title: 'Tote bag',
