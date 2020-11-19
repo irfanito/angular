74d9d8d673852c7a0fe75ae4611933fbc8ce7333
SALIM Irfane <irfane.salim@generali.com>
2020-11-19 07:53:23 -0800

    Implements SortByTitlePipe

--- src/app/sort-by-title.pipe.spec.ts
+++ src/app/sort-by-title.pipe.spec.ts
@@ -1,8 +1,34 @@
+import { Product } from './model/model/product';
 import { SortByTitlePipe } from './sort-by-title.pipe';
 
 describe('SortByTitlePipe', () => {
+  let pipe: SortByTitlePipe;
+  
+  beforeEach(()=>{
+    pipe =new SortByTitlePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new SortByTitlePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should sort products by title', () => {
+    // given
+    const param = [initProduct('c'), initProduct('a'), initProduct('b')];
+    const expected = [initProduct('a'), initProduct('b'), initProduct('c')];
+    // when
+    const actual = pipe.transform(param);
+    // then
+    expect(actual).toEqual(expected);
+  });
 });
+
+function initProduct(title: string): Product {
+  return {
+    title,
+    description: 'description',
+    photo: 'photo',
+    price: 3,
+    stock: 2
+  };
+}
--- src/app/sort-by-title.pipe.ts
+++ src/app/sort-by-title.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from './model/model/product';
 
 @Pipe({
   name: 'sortByTitle'
 })
 export class SortByTitlePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: Product[]): Product[] {
+    return value
+      .sort((product1, product2) => product1.title.localeCompare(product2.title));
   }
 
 }
