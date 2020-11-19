11b0c95d4a75d92dbdc43a85d0ea49e2585622df
SALIM Irfane <irfane.salim@generali.com>
2020-11-19 07:24:59 -0800

    Configure locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ProductComponent } from './product/product.component';
 import { ProductService } from './services/product.service';
 import { CustomerService } from './services/customer.service';
+
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
