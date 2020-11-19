377e1aec06388e2f7e9c6b943a89176903408926
SALIM Irfane <irfane.salim@generali.com>
2020-11-19 05:34:33 -0800

    Inject ProductService and CustomerService in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Product } from './model/model/product';
 import { defaultProducts } from './products';
+import { CustomerService } from './services/customer.service';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ProductService,CustomerService]
 })
 export class AppComponent {
   title: string = 'This is my first component';
   total: number;
   products: Product[];
   
-  constructor(){
+  constructor(private productService: ProductService, private customerService: CustomerService){
     this.total = 0;
     this.products = defaultProducts;
   }
