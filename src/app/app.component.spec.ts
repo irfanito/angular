65ef218b877b69f51e6acf6f297c93b40a3a3bc6
Irfane Salim <isalim@generali.fr>
2020-11-20 20:43:06 +0100

    Revert "Move app component code to home component"
    This reverts commit 96d282cf

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,35 @@
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {Observable, of} from 'rxjs';
 import {AppComponent} from './app.component';
+import {Product} from './model/model/product';
+import {ProductComponent} from './product/product.component';
+import {defaultProducts} from './products';
+import {CustomerService} from './services/customer.service';
+import {ProductService} from './services/product.service';
+import {SortByPipe} from './sort-by.pipe';
 
 describe('AppComponent', () => {
   let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let customerServiceStub;
+  let productServiceStub;
 
   beforeEach(async () => {
+    customerServiceStub = jasmine.createSpyObj('customerServiceStub', ['getTotal']);
+    productServiceStub = jasmine.createSpyObj('productServiceStub', ['getProducts', 'isAvailable', 'isTheLast']);
+    customerServiceStub.getTotal.and.returnValue(of(0));
+    defaultMock(productServiceStub);
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ProductComponent,
+        SortByPipe
+      ],
+      providers: [
+        {provide: CustomerService, useValue: customerServiceStub},
+        {provide: ProductService, useValue: productServiceStub}
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
@@ -24,4 +44,45 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     expect(app).toBeTruthy();
   });
+
+  it(`should pass products with stock greather than 0 to ProductComponent childs`, () => {
+    // mock
+    const productsObservable: Observable<Product[]> = productServiceStub.getProducts();
+    productsObservable.subscribe(products => {
+      productServiceStub.isAvailable
+        .withArgs(products[0])
+        .and
+        .returnValue(false);
+      // expected
+      let [firstProduct, ...expected] = products;
+      // when
+      fixture.detectChanges();
+      // then
+      const actual: Product[] = fixture.debugElement.queryAll(By.directive(ProductComponent))
+        .map(debugElement => debugElement.componentInstance.data);
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it(`should sort by price when click on price button`, () => {
+    // mock
+    const [product0, product1, product2, product3]: Product[] = defaultProducts;
+    // expected
+    let expected: Product[] = [product3, product2, product0, product1];
+    // when
+    const priceButton: HTMLImageElement = fixture.debugElement.query(By.css('#priceButton')).nativeElement;
+    priceButton.click();
+    fixture.detectChanges();
+    // then
+    const actual: Product[] = fixture.debugElement.queryAll(By.directive(ProductComponent))
+      .map(debugElement => debugElement.componentInstance.data);
+    expect(actual).toEqual(expected);
+  });
 });
+
+function defaultMock(productServiceStub: any) {
+  productServiceStub.getProducts.and.returnValue(of([...defaultProducts]));
+  productServiceStub.isAvailable.and.returnValue(true);
+  productServiceStub.isTheLast.and.returnValue(true);
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Product} from './model/model/product';
+import {CustomerService} from './services/customer.service';
+import {ProductService} from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title: string = 'This is my first component';
+  total: number;
+  sortPropertyName: string = 'title';
+  sortPropertyNames: string[] = ['title', 'price', 'stock'];
+  products$: Observable<Product[]>;
 
+  constructor(private productService: ProductService, private customerService: CustomerService) {
+  }
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts();
+    this.updateTotal();
+  }
+
+  public showProduct(product: Product): boolean {
+    return this.productService.isAvailable(product);
+  }
+
+  public updateTotal() {
+    this.customerService.getTotal().subscribe(res => {
+      this.total = res;
+    });
+  }
+
+  public onSortByButtonClick(propertyName: string): void {
+    this.sortPropertyName = propertyName;
+  }
 }
