c113f9e8d8fa4c684df25d4037ed9de84bd091d6
Irfane Salim <isalim@generali.fr>
2020-11-20 20:44:42 +0100

    Revert "BasketComponent and HomeComponent are copies of AppComponent"
    This reverts commit 75552c17

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {Product} from '../model/model/product';
-import {CustomerService} from '../services/customer.service';
-import {ProductService} from '../services/product.service';
 
 @Component({
   selector: 'app-basket',
@@ -10,31 +6,11 @@ import {ProductService} from '../services/product.service';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent implements OnInit {
-  title: string = 'This is my first component';
-  total: number;
-  sortPropertyName: string = 'title';
-  sortPropertyNames: string[] = ['title', 'price', 'stock'];
-  products$: Observable<Product[]>;
 
-  constructor(private productService: ProductService, private customerService: CustomerService) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.products$ = this.productService.getProducts();
-    this.updateTotal();
   }
 
-  public showProduct(product: Product): boolean {
-    return this.productService.isAvailable(product);
-  }
-
-  public updateTotal() {
-    this.customerService.getTotal().subscribe(res => {
-      this.total = res;
-    });
-  }
-
-  public onSortByButtonClick(propertyName: string): void {
-    this.sortPropertyName = propertyName;
-  }
 }
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,90 +1,25 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { of, Observable } from 'rxjs';
-import { Product } from '../model/model/product';
-import { ProductComponent } from '../product/product.component';
-import { defaultProducts } from '../products';
-import { CustomerService } from '../services/customer.service';
-import { ProductService } from '../services/product.service';
-import { SortByPipe } from '../sort-by.pipe';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
 
 describe('HomeComponent', () => {
-  let homeComponent: HomeComponent;
+  let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-  let customerServiceStub;
-  let productServiceStub;
 
   beforeEach(async () => {
-    customerServiceStub = jasmine.createSpyObj('customerServiceStub', ['getTotal']);
-    productServiceStub = jasmine.createSpyObj('productServiceStub', ['getProducts', 'isAvailable', 'isTheLast']);
-    customerServiceStub.getTotal.and.returnValue(of(0));
-    defaultMock(productServiceStub);
     await TestBed.configureTestingModule({
-      declarations: [
-        HomeComponent,
-        ProductComponent,
-        SortByPipe
-      ],
-      providers: [
-        {provide: CustomerService, useValue:customerServiceStub},
-        {provide: ProductService, useValue:productServiceStub}
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
+      declarations: [HomeComponent]
+    })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
-    homeComponent = fixture.componentInstance;
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    expect(homeComponent).toBeTruthy();
-  });
-
-  it(`should pass products with stock greather than 0 to ProductComponent childs`, () => {
-    // mock
-    const productsObservable: Observable<Product[]> = productServiceStub.getProducts();
-    productsObservable.subscribe(products => {
-      productServiceStub.isAvailable
-      .withArgs(products[0])
-      .and
-      .returnValue(false);
-      // expected
-      let [firstProduct, ...expected] = products;
-      // when
-      fixture.detectChanges();
-      // then
-      const actual: Product[] = fixture.debugElement.queryAll(By.directive(ProductComponent))
-        .map(debugElement => debugElement.componentInstance.data);
-      expect(actual).toEqual(expected);
-    });
-  });
-
-  it(`should sort by price when click on price button`, () => {
-    // mock
-    const [product0, product1, product2, product3]: Product[] = defaultProducts;
-    // expected
-    let expected: Product[] = [product3, product2, product0, product1];
-    // when
-    const priceButton: HTMLImageElement = fixture.debugElement.query(By.css('#priceButton')).nativeElement;
-    priceButton.click();
-    fixture.detectChanges();
-      // then
-    const actual: Product[] = fixture.debugElement.queryAll(By.directive(ProductComponent))
-      .map(debugElement => debugElement.componentInstance.data);
-    expect(actual).toEqual(expected);
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
-
-function defaultMock(productServiceStub: any) {
-  productServiceStub.getProducts.and.returnValue(of([...defaultProducts]));
-  productServiceStub.isAvailable.and.returnValue(true);
-  productServiceStub.isTheLast.and.returnValue(true);
-}
-
-
