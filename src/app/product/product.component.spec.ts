22fdc45f232da6a3e83ca7f49c8bb0bed4229995
Irfane Salim <isalim@generali.fr>
2020-11-21 19:48:23 +0100

    Refactor product component test

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -42,33 +42,6 @@ describe('ProductComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should bind title and price property to h3', () => {
-    const h3: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
-    expect(h3.textContent).toBe('title - €3.00');
-  });
-
-  it('should bind photo property to img.src', () => {
-    const img: HTMLImageElement = fixture.debugElement.query(By.css('img')).nativeElement;
-    expect(img.src.endsWith('photo')).toBeTruthy();
-  });
-
-  it('should emit addToBasket event when click on button', () => {
-    // given
-    spyOn(component.addToBasket, 'emit');
-    const button: HTMLImageElement = fixture.debugElement.query(By.css('button')).nativeElement;
-    // when
-    button.click();
-    // then
-    expect(component.addToBasket.emit).toHaveBeenCalledOnceWith(component.data);
-  });
-
-  it(`should onAddToBasket call productService.decreaseStock with data`, () => {
-    // when
-    component.onAddToBasket();
-    // then
-    expect(productServiceStub.decreaseStock).toHaveBeenCalledOnceWith(component.data);
-  });
-
   it('should use last css class when productService.isTheLast return true', () => {
     // given
     productServiceStub.isTheLast.and.returnValue(true);
@@ -87,5 +60,37 @@ describe('ProductComponent', () => {
     // then
     const thumbnailLastDebugElements: DebugElement[] = fixture.debugElement.queryAll(By.css('.thumbnail.last'));
     expect(thumbnailLastDebugElements.length).toBeFalsy();
+  });
+
+  it('should bind photo property to img[src]', () => {
+    const img: HTMLImageElement = fixture.debugElement.query(By.css('img')).nativeElement;
+    expect(img.src.endsWith('photo')).toBeTruthy();
+  });
+
+  it('should bind title and price property to h3', () => {
+    const h3: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+    expect(h3.textContent).toBe('title - €3.00');
+  });
+
+  it('should bind description to first p', () => {
+    const h3: HTMLElement = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(h3.textContent).toBe('description');
+  });
+
+  it(`should onAddToBasket call productService.decreaseStock with data`, () => {
+    // when
+    component.onAddToBasket();
+    // then
+    expect(productServiceStub.decreaseStock).toHaveBeenCalledOnceWith(component.data);
+  });
+
+  it('should emit addToBasket event when click on button', () => {
+    // given
+    spyOn(component.addToBasket, 'emit');
+    const button: HTMLImageElement = fixture.debugElement.query(By.css('button')).nativeElement;
+    // when
+    button.click();
+    // then
+    expect(component.addToBasket.emit).toHaveBeenCalledOnceWith(component.data);
   });
 });
